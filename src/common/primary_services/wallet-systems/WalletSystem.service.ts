ee7733f3fa788a68e29dc86bdcd81613692a41b4
miracle boniface <udohmiracle23@gmail.com>
2025-10-01 15:10:20 +0100

    repo updated

--- src/common/primary_services/wallet-systems/WalletSystem.service.ts
+++ src/common/primary_services/wallet-systems/WalletSystem.service.ts
@@ -460,8 +460,8 @@ export class WalletSystemService {
       const stakeGasPrice = feeData.maxFeePerGas ?? feeData.gasPrice!;
       const stakeRequiredETH = approveGas * stakeGasPrice;
       const stakeEthBalance = await this.provider.getBalance(wallet.address);
-      if (stakeEthBalance < stakeRequiredETH) {
-         throw new InternalServerErrorException(`Insufficient ETH for gas. Required: ${ethers.formatEther(stakeRequiredETH)}, Available: ${ethers.formatEther(stakeEthBalance)}`);
+      if (stakeGasPrice < stakeRequiredETH) {
+         throw new InternalServerErrorException(`Insufficient ETH for gas. Required: ${ethers.formatEther(stakeGasPrice)}, Available: ${ethers.formatEther(stakeEthBalance)}`);
       }
       console.log('sufficient eth for stake gas')
 
